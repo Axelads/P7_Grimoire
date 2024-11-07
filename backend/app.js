e43f16e2442e5d98431c68ea3726a4c9633b66af
Axel Gregoire <professionnel.agregoire@gmail.com>
2024-11-07 13:20:44 +0100

    changement avec le .env

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+require('dotenv').config(); // Charge le fichier .env
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -8,29 +9,28 @@ const booksRoutes = require('./routes/books');
 
 const app = express();
 
-// Connexion à MongoDB avec la variable d'environnement
-mongoose.connect(process.env.MONGO_URI)
+// Connexion à MongoDB
+mongoose.connect(process.env.MONGODB_URL)
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch((error) => console.error('Connexion à MongoDB échouée !', error));
+  .catch((error) => console.log('Connexion à MongoDB échouée :', error));
 
+// Middleware
 app.use(bodyParser.json());
 
 // CORS
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Autorise uniquement le frontend
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   if (req.method === 'OPTIONS') {
-    return res.sendStatus(200); // Gérer la requête préliminaire
+    return res.sendStatus(200);
   }
   next();
 });
 
-// Utilisation des routes d'authentification
+// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/books', booksRoutes);
-
-// Utiliser le chemin absolu pour servir le dossier imageAdd grâce à path.join
 app.use('/imagesAdd', express.static(path.join(__dirname, 'imagesAdd')));
 
 module.exports = app;
